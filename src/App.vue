43e726536a77ca1d7b27df3cee27fa203290aea6
jstiven01 <stiven10@gmail.com>
2020-02-24 14:46:12 -0500

    Creating MovieForm

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MovieForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MovieForm from './components/MovieForm'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MovieForm
   }
 }
 </script>
---
+++ src/components/MovieForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="list">
+        this is a form
+        <form @submit.prevent="addMovie">
+            <input type="text" name="title" id="nameMovie" v-model="title" placeholder="Insert Name Movie">
+            <button type="submit">Name search movie</button>
+        </form>
+        <div>{{title}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MovieForm", 
+    data(){
+        return {
+            title:'',
+        }
+    },
+    methods:{
+        addMovie(){
+            console.log("hey pal")
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
