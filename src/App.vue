5a8d431efd21c8497f2557658d7a5873141d13b1
jstiven01 <stiven10@gmail.com>
2020-02-27 18:00:25 -0500

    rendering datatable

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,46 @@
   <div id="app">
     <MovieForm />
     <MovieList />
-    <Pagination />
+    <DataTable :repositories="repositories" />
 
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import MovieForm from './components/MovieForm.vue';
 import MovieList from './components/MovieList.vue';
-import Pagination from './components/Pagination.vue';
+import DataTable from './components/DataTable.vue';
+
+const githubAPI = 'https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc&per_page=100&page=';
 
 export default {
   name: 'App',
   components: {
     MovieForm,
     MovieList,
-    Pagination,
+    DataTable,
+  },
+  data() {
+    return {
+      githubPage: 1,
+      repositories: [],
+    };
+  },
+
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios.get(githubAPI + this.githubPage)
+        .then(({ data }) => {
+          this.repositories = this.repositories.concat(data.items);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
 };
 </script>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,41 @@
+<template>
+  <table class="table">
+
+    <tbody>
+      <tr v-for="repo in repositories" :key="repo.id">
+        <th scope="row">
+          {{ repositories.indexOf(repo) + 1 }}
+        </th>
+        <td>
+          {{ repo.name }}
+        </td>
+        <td>
+          {{ repo.stargazers_count.toLocaleString() }}
+        </td>
+        <td>
+          <img v-if="repo.owner.avatar_url" class="avatar" :src="repo.owner.avatar_url"/>
+        </td>
+        <td>
+          <a :href="repo.html_url" target="_blank">Link</a>
+        </td>
+        <td>
+          <a v-if="repo.homepage" :href="repo.homepage" target="_blank">Homepage</a>
+          <p v-else>No homepage</p>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    repositories: Array,
+  },
+};
+</script>
+<style scoped>
+  .avatar {
+    max-width: 50px;
+  }
+</style>
